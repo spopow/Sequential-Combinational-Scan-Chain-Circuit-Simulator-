bdbb49bd0e592ec116544c18bc18301d5894dc35
alexisblopez <alopez78@uic.edu>
2019-12-02 16:29:23 -0600

    added scan chain study

--- genFaultList.py
+++ genFaultList.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from __future__ import print_function
+import random
 
 
 # OVERVIEW
@@ -109,6 +110,82 @@ def getFaultList(netName):
     selectedFault = faults[userChoice - 1]
 
     return selectedFault
+
+
+# FIXME change to get 15 random faults from the fault list
+def getFaultListStudy(netName):
+    # Opening the netlist file:
+    netFile = open(netName, "r")
+
+    # temporary variables
+    faults = []  # array of the faults
+
+    selectedFaults = []
+
+    # Reading in file line by line
+    for line in netFile:
+
+        # NOT Reading any empty lines
+        if (line == "\n"):
+            continue
+
+        # Removing spaces and newlines
+        line = line.replace(" ", "")
+        line = line.replace("\n", "")
+
+        # NOT Reading any comments
+        if (line[0] == "#"):
+            continue
+
+        # Read INPUTs and add faults to list:
+        if (line[0:5] == "INPUT"):
+            # Removing everything but the line variable name
+            line = line.replace("INPUT", "")
+            line = line.replace("(", "")
+            line = line.replace(")", "")
+
+            # Format the variable name for *VAR_NAME*-SA-0 and *VAR_NAME*-SA-1
+            stuckAt0 = line + "-SA-0"
+            stuckAt1 = line + "-SA-1"
+
+            # Appending to the fault array
+            faults.append(stuckAt0)
+            faults.append(stuckAt1)
+            continue
+
+        # Read an OUTPUT wire
+        # Note that the same wire should also appear somewhere else as a GATE output, so won't do anything with it.
+        if line[0:6] == "OUTPUT":
+            continue
+
+        # Read a gate output wire, and add to the circuit dictionary
+        lineSpliced = line.split("=")  # splicing the line at the equals sign to get the gate output wire
+        stuckAt0 = lineSpliced[0] + "-SA-0"
+        stuckAt1 = lineSpliced[0] + "-SA-1"
+        # Appending faults to the fault list
+
+        faults.append(stuckAt0)
+        faults.append(stuckAt1)
+
+        # splicing the line again at the "("  to get the gate logic
+        lineSplicedAgain = lineSpliced[1].split("(")
+
+        lineSplicedAgain[1] = lineSplicedAgain[1].replace(")", "")
+
+        # Splicing the the line again at each comma to the get the gate terminals
+        inputs = lineSplicedAgain[1].split(",")
+
+        # Turning each term into an integer before putting it into the circuit dictionary
+        for signal in inputs:
+            stuckAt0 = lineSpliced[0] + "-IN-" + signal + "-SA-0"
+            stuckAt1 = lineSpliced[0] + "-IN-" + signal + "-SA-1"
+            faults.append(stuckAt0)
+            faults.append(stuckAt1)
+
+    # for _ in range(numberFaults):
+        # selectedFaults.append(random.choice(faults))
+
+    return faults
 
 
 # If this module is executed alone
